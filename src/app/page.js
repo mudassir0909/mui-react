1bc32f7febe09aa7b29d6a01d6eb64ae4fd06405
Mudassir Ali <mudassir.dev@gmail.com>
2023-11-26 12:49:24 +1100

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,102 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Paper from '@mui/material/Paper';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Box sx={{ margin: 2 }}>
+          <ListOfBooks />
+        </Box>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <AppNavigation />
+      </Container>
+    </React.Fragment>
+  );
+}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+function AppNavigation() {
+  return (
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation
+        showLabels
+      >
+        <BottomNavigationAction label="Books" icon={<AutoStoriesIcon />} />
+        <BottomNavigationAction label="Food" icon={<DinnerDiningIcon />} />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      </BottomNavigation>
+    </Paper>
+  );
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+const books = [
+  { title: 'Programming in C++', author: 'Balaguruswamy' },
+  { title: 'Learning C', author: "Author 2" },
+  { title: 'Adventures of C++', author: 'Auhtor 3' },
+  { title: 'Learning Javascript', author: 'Author 4' },
+  { title: 'Programming in C++', author: 'Balaguruswamy' },
+  { title: 'Learning C', author: "Author 2" },
+  { title: 'Adventures of C++', author: 'Auhtor 3' },
+  { title: 'Learning Javascript', author: 'Author 4' },
+  { title: 'Programming in C++', author: 'Balaguruswamy' },
+  { title: 'Learning C', author: "Author 2" },
+  { title: 'Adventures of C++', author: 'Auhtor 3' },
+  { title: 'Learning Javascript', author: 'Author 4' },
+  { title: 'Programming in C++', author: 'Balaguruswamy' },
+  { title: 'Learning C', author: "Author 2" },
+  { title: 'Adventures of C++', author: 'Auhtor 3' },
+  { title: 'Learning Javascript', author: 'Author 4' },
+];
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+function ListOfBooks() { // 12/3 = 4
+  return (
+    <Grid container spacing={2}>
+      {books.map((book, idx) => (
+        <Grid item xs={3} index={idx}>
+          <BookCard title={book.title} author={book.author} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+function BookCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="https://m.media-amazon.com/images/I/41FQ-HlNyRL.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.author}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          FREE
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Buy</Button>
+      </CardActions>
+    </Card>
+  );
 }
